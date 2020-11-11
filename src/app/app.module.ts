0bf4178ed90b5f7bf6a02f069b0172d3c9b78524
ANISH ARALIKATTI (anaralik) <anaralik@cisco.com>
2020-11-11 12:20:20 +0530

    Added Angular modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AdminComponent,
+    UserComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
